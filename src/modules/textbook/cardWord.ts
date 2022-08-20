898b3a0a3edcecfdf678f18e57aaf4811c7b926a
Ulugbek <u.dushanov@gmail.com>
2022-08-20 11:55:45 +0500

    feat: add function to playing auido files on one click

--- src/modules/textbook/cardWord.ts
+++ src/modules/textbook/cardWord.ts
@@ -2,25 +2,27 @@ import { GetWords } from './requestWords';
 
 export default function cardWord(result: GetWords[]) {
     let card = '';
-    result.forEach((item) => {
+    result.forEach((item, i) => {
         card += `
             <div class="words__card">
                 <img class="word__img" src="https://react-learnwords-english.herokuapp.com/${item.image}" alt="word image">
                 <div class="word__card--right">
                     <h2 class="word__title">${item.word} ${item.transcription}
-                        <audio src="https://react-learnwords-english.herokuapp.com/${item.audio}" controls></audio>
-                        <span class="material-symbols-outlined">
+                        <span class="material-symbols-outlined" data-volume=${i}>
                             volume_up
                         </span>
                     </h2>
+                    <div class="words__audio" data-audio=${i}>
+                        <audio src="https://react-learnwords-english.herokuapp.com/${item.audio}"></audio>
+                        <audio src="https://react-learnwords-english.herokuapp.com/${item.audioMeaning}"></audio>
+                        <audio src="https://react-learnwords-english.herokuapp.com/${item.audioExample}"></audio>
+                    </div>
                     <p class="word__translate">${item.wordTranslate}</p>
                     <br>
                     <p>${item.textMeaning}</p>
-                    <audio src="https://react-learnwords-english.herokuapp.com/${item.audioMeaning}" controls></audio>
                     <p class="word__translate">${item.textMeaningTranslate}</p>
                     <br>
                     <p>${item.textExample}</p>
-                    <audio src="https://react-learnwords-english.herokuapp.com/${item.audioExample}" controls></audio>
                     <p class="word__translate">${item.textExampleTranslate}</p>
                 </div>
             </div>
--- src/modules/textbook/renderCardWords.ts
+++ src/modules/textbook/renderCardWords.ts
@@ -7,8 +7,33 @@ import paginationWords from './paginationWords';
 export default function renderCardWords() {
     const textbookWords = document.querySelector('.textbook__words') as HTMLDivElement;
     getWords(storage.groupCount, storage.pageCount).then((result) => {
-        console.log(result)
         textbookWords.innerHTML = cardWord(result);
+
+        textbookWords.addEventListener('click', (event) => {
+            const target = event.target as HTMLElement;
+
+            if (target.dataset.volume) {
+                const selector = `[data-audio="${target.dataset.volume}"]`;
+                const wordsAudio = document.querySelector(selector) as HTMLDivElement;
+                const audiofirst = (wordsAudio.children[0] as HTMLAudioElement).src;
+                const audiosecond = (wordsAudio.children[1] as HTMLAudioElement).src;
+                const audiothird = (wordsAudio.children[2] as HTMLAudioElement).src;
+                const audioArr = [audiofirst, audiosecond, audiothird];
+                const audio = new Audio(audioArr[0]);
+
+                audio.src = audioArr[0];
+                audio.play();
+
+                let index = 1;
+                audio.onended = function () {
+                    if (index < audioArr.length) {
+                        audio.src = audioArr[index];
+                        audio.play();
+                        index++;
+                    }
+                };
+            }
+        });
     });
 
     groupWords();
