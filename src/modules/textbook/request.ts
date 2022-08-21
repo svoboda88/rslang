c3313cd9ca8746b732a3cfa071939fb3b44141bd
Ulugbek <96809764+udushanov@users.noreply.github.com>
2022-08-21 13:49:07 +0500

    refactor: rename request functions

--- src/modules/textbook/request.ts
+++ src/modules/textbook/request.ts
@@ -17,13 +17,13 @@ export interface GetWords {
     textExampleTranslate: 'string';
 }
 
-export const getWords = async (group: number, page: number): Promise<GetWords[]> => {
+export const getWordsResult = async (group: number, page: number): Promise<GetWords[]> => {
     const response = await fetch(`${base}?group=${group}&page=${page}`);
     const items = await response.json();
     return items;
 };
 
-export const getWord = async (id: string): Promise<GetWords> => {
+export const getWordResult = async (id: string): Promise<GetWords> => {
     const response = await fetch(`${base}/${id}`);
     const item = await response.json();
     return item;
