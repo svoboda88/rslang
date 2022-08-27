3da4b4b1caebbc4febde27e66a7065976d992534
Ulugbek <u.dushanov@gmail.com>
2022-08-27 16:00:32 +0500

    feat: add method to show results

--- src/modules/audiocall/audiocall.ts
+++ src/modules/audiocall/audiocall.ts
@@ -64,6 +64,7 @@ export class Audiocall {
     levelSelection() {
         if (this.audiocallLvls) {
             this.audiocallLvls.addEventListener('click', (event) => {
+                event.stopImmediatePropagation();
                 const target = event.target as HTMLDivElement;
                 if (target.classList.contains('audiocall-lvls__btn')) {
                     const groupCount = Number(target.dataset.callLvl);
@@ -90,89 +91,110 @@ export class Audiocall {
                 storage.wordIndex = index;
                 this.voiceBtn?.addEventListener('click', (event) => {
                     event.stopImmediatePropagation();
-                    this.wordVoice(storage.wordVariants, storage.wordIndex);
+                    this.wordVoice();
                 });
-                this.wordVoice(sliced, index);
+                this.wordVoice();
                 let resultWords = '';
-                sliced.forEach((item, i) => {
+                storage.wordVariants.forEach((item, i) => {
                     resultWords += `
                         <div class="audiocall__word-btn" data-call-word=${i}>${item.wordTranslate}</div>
                     `;
                 });
                 (this.audiocallWords as HTMLDivElement).innerHTML = resultWords;
-                this.answerWord(sliced, index);
+                this.answerWord();
                 this.nextBtn?.addEventListener('click', (event) => {
                     event.stopImmediatePropagation();
-                    (this.wordImg as HTMLDivElement).innerHTML = '';
-                    (this.correctWord as HTMLDivElement).textContent = '';
-                    this.voiceBtn?.classList.remove('smaller');
-                    this.startGame();
+                    if ((this.nextBtn as HTMLDivElement).textContent === 'Не знаю') {
+                        this.showCorrectWord();
+                        storage.wrongAnswers.push({
+                            audio: storage.wordVariants[storage.wordIndex].audio,
+                            word: storage.wordVariants[storage.wordIndex].word,
+                            translate: storage.wordVariants[storage.wordIndex].wordTranslate,
+                        });
+                        this.showResult();
+                    } else {
+                        this.hideCorrectWord();
+                        this.startGame();
+                    }
                 });
-                this.showResult();
             });
         }
     }
 
-    wordVoice(arr: GetWords[], i: number) {
-        const audioSource = `https://react-learnwords-english.herokuapp.com/${arr[i].audio}`;
+    showCorrectWord() {
+        (this.nextBtn as HTMLDivElement).textContent = 'Дальше';
+        (this.wordImg as HTMLDivElement).innerHTML = `
+            <img src="https://react-learnwords-english.herokuapp.com/${storage.wordVariants[storage.wordIndex].image}">
+        `;
+        (this.correctWord as HTMLDivElement).textContent = storage.wordVariants[storage.wordIndex].word;
+        (this.audiocallWords?.children[storage.wordIndex] as HTMLDivElement).style.backgroundColor = 'green';
+        this.voiceBtn?.classList.add('smaller');
+    }
+
+    hideCorrectWord() {
+        (this.nextBtn as HTMLDivElement).textContent = 'Не знаю';
+        (this.wordImg as HTMLDivElement).innerHTML = '';
+        (this.correctWord as HTMLDivElement).textContent = '';
+        this.voiceBtn?.classList.remove('smaller');
+    }
+
+    wordVoice() {
+        const audioSource = `
+            https://react-learnwords-english.herokuapp.com/${storage.wordVariants[storage.wordIndex].audio}
+        `;
         const audio = new Audio(audioSource);
         audio.play();
     }
 
-    answerWord(arr: GetWords[], i: number) {
+    answerWord() {
         if (this.audiocallWords) {
             this.audiocallWords.addEventListener('click', (event) => {
+                event.stopImmediatePropagation();
                 const target = event.target as HTMLDivElement;
                 const choosenWord = Number(target.dataset.callWord);
                 Array.from(this.audiocallWords?.children as HTMLCollection).forEach((item: Element) => {
                     (item as HTMLDivElement).style.backgroundColor = '';
                 });
-                (this.audiocallWords?.children[i] as HTMLDivElement).style.backgroundColor = 'green';
-
-                if (choosenWord === i) {
-                    arr.forEach((item) => {
-                        if (item.word === target.textContent) {
-                            storage.correctAnswers.push({
-                                audio: item.audio,
-                                word: item.word,
-                                translate: item.wordTranslate,
-                            });
-                        }
+                (this.audiocallWords?.children[storage.wordIndex] as HTMLDivElement).style.backgroundColor = 'green';
+                if (choosenWord === storage.wordIndex) {
+                    storage.correctAnswers.push({
+                        audio: storage.wordVariants[storage.wordIndex].audio,
+                        word: storage.wordVariants[storage.wordIndex].word,
+                        translate: storage.wordVariants[storage.wordIndex].wordTranslate,
                     });
-                    (this.nextBtn as HTMLDivElement).textContent = 'Дальше';
-                    (this.wordImg as HTMLDivElement).innerHTML = `
-                        <img src="https://react-learnwords-english.herokuapp.com/${arr[i].image}">
-                    `;
-                    (this.correctWord as HTMLDivElement).textContent = arr[i].word;
-                    this.voiceBtn?.classList.add('smaller');
                 } else {
                     target.style.backgroundColor = 'red';
-                    arr.forEach((item) => {
-                        if (item.word === target.textContent) {
-                            storage.wrongAnswers.push({
-                                audio: item.audio,
-                                word: item.word,
-                                translate: item.wordTranslate,
-                            });
-                        }
+                    storage.wrongAnswers.push({
+                        audio: storage.wordVariants[storage.wordIndex].audio,
+                        word: storage.wordVariants[storage.wordIndex].word,
+                        translate: storage.wordVariants[storage.wordIndex].wordTranslate,
                     });
-                    (this.nextBtn as HTMLDivElement).textContent = 'Дальше';
-                    (this.wordImg as HTMLDivElement).innerHTML = `
-                        <img src="https://react-learnwords-english.herokuapp.com/${arr[i].image}">
-                    `;
-                    (this.correctWord as HTMLDivElement).textContent = arr[i].word;
-                    this.voiceBtn?.classList.add('smaller');
                 }
+                this.showCorrectWord();
+                this.showResult();
             });
         }
     }
 
     showResult() {
         const answersSum = storage.correctAnswers.length + storage.wrongAnswers.length;
-        console.log(answersSum);
+        console.log(storage.correctAnswers, storage.wrongAnswers);
         if (answersSum === 10) {
             this.gameWindow?.classList.add('hidden');
             this.gameResults?.classList.remove('hidden');
+            this.gameResults?.addEventListener('click', (event) => {
+                event.stopImmediatePropagation();
+                const target = event.target as HTMLElement;
+                if (target.classList.contains('results__play-btn')) {
+                    this.gameResults?.classList.add('hidden');
+                    this.audiocallLvls?.classList.remove('hidden');
+                    storage.correctAnswers = [];
+                    storage.wrongAnswers = [];
+                    this.hideCorrectWord();
+                }
+
+                return;
+            });
         }
     }
 }
